f1ef111e54630241ef05f74b379ea8d0c07a2c7e
Rob Hoelz <rob@hoelz.ro>
2011-06-03 03:58:24 +0000

    Import of RHOELZ/Bash-Completion-Plugins-perlbrew-0.02-TRIAL from CPAN.
    gitpan-cpan-distribution: Bash-Completion-Plugins-perlbrew
    gitpan-cpan-version:      0.02-TRIAL
    gitpan-cpan-path:         RHOELZ/Bash-Completion-Plugins-perlbrew-0.02-TRIAL.tar.gz
    gitpan-cpan-author:       RHOELZ
    gitpan-cpan-maturity:     developer

--- lib/Bash/Completion/Plugins/perlbrew.pm
+++ lib/Bash/Completion/Plugins/perlbrew.pm
@@ -1,5 +1,10 @@
+## no critic (RequireUseStrict)
 package Bash::Completion::Plugins::perlbrew;
+BEGIN {
+  $Bash::Completion::Plugins::perlbrew::VERSION = '0.02'; # TRIAL
+}
 
+## use critic (RequireUseStrict)
 use strict;
 use warnings;
 use feature 'switch';
@@ -7,10 +12,10 @@ use parent 'Bash::Completion::Plugin';
 
 use Bash::Completion::Utils qw(command_in_path);
 
-our $VERSION = '0.01';
-
 my @perlbrew_commands = qw/
-init install list use switch mirror off version help env install-cpanm
+init    install list use           switch    mirror    off
+version help    env  install-cpanm available uninstall self-upgrade
+alias
 /;
 
 my @perlbrew_options = qw/
@@ -40,13 +45,15 @@ sub complete {
                 $r->candidates(grep { /^\Q$word\E/ }
                     ( @perlbrew_commands, @perlbrew_options ));
             }
-            when(qr/^switch|env|use$/) {
+            when(qr/^switch|env|use|uninstall|alias$/) {
                 my @perls = split /\n/, qx(perlbrew list);
                 @perls = map { /^\*?\s*(?<name>\S+)/; $+{'name'} } @perls;
                 $r->candidates(grep { /^\Q$word\E/ } @perls);
             }
             when('install') {
-                continue; # for now (complete Perls later)
+                my @perls = split /\n/, qx(perlbrew available);
+                @perls = map { /^i?\s*(?<name>.*)/; $+{'name'}  } @perls;
+                $r->candidates(grep { /^\Q$word\E/ } @perls);
             }
             default {
                 # all other commands (including unrecognized ones) get
@@ -59,7 +66,9 @@ sub complete {
 
 1;
 
-__END__
+
+
+=pod
 
 =head1 NAME
 
@@ -67,29 +76,12 @@ Bash::Completion::Plugins::perlbrew - Bash completion for perlbrew
 
 =head1 VERSION
 
-0.01
+version 0.02
 
 =head1 DESCRIPTION
 
 L<Bash::Completion> support for L<perlbrew|App::perlbrew>.  Completes perlbrew
 options as well as installed perlbrew versions.
-
-=head1 AUTHOR
-
-Rob Hoelz C<< rob at hoelz.ro >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-Bash-Completion-Plugins-perlbrew at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Bash-Completion-Plugins-perlbrew>. I will
-be notified, and then you'll automatically be notified of progress on your bug as I make changes.
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2011 Rob Hoelz.
-
-This module is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself.
 
 =head1 SEE ALSO
 
@@ -107,4 +99,30 @@ L<Bash::Completion>, L<Bash::Completion::Plugin>, L<App::perlbrew>
 
 =end comment
 
+=head1 AUTHOR
+
+Rob Hoelz <rob@hoelz.ro>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Rob Hoelz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+http://github.com/hoelzro/bash-completion-plugins-perlbrew/issues
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =cut
+
+
+__END__
+
+# ABSTRACT: Bash completion for perlbrew
+
