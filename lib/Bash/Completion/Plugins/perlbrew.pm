55905f5054303defc0d0d5565f8c86734d67ce4a
Rob Hoelz <rob@hoelz.ro>
2012-03-28 23:12:32 +0000

    Import of RHOELZ/Bash-Completion-Plugins-perlbrew-0.05 from CPAN.
    gitpan-cpan-distribution: Bash-Completion-Plugins-perlbrew
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         RHOELZ/Bash-Completion-Plugins-perlbrew-0.05.tar.gz
    gitpan-cpan-author:       RHOELZ
    gitpan-cpan-maturity:     released

--- lib/Bash/Completion/Plugins/perlbrew.pm
+++ lib/Bash/Completion/Plugins/perlbrew.pm
@@ -1,7 +1,7 @@
 ## no critic (RequireUseStrict)
 package Bash::Completion::Plugins::perlbrew;
-BEGIN {
-  $Bash::Completion::Plugins::perlbrew::VERSION = '0.04';
+{
+  $Bash::Completion::Plugins::perlbrew::VERSION = '0.05';
 }
 
 ## use critic (RequireUseStrict)
@@ -15,15 +15,48 @@ use Bash::Completion::Utils qw(command_in_path);
 my @perlbrew_commands = qw/
 init    install list use           switch    mirror    off
 version help    env  install-cpanm available uninstall self-upgrade
-alias
+alias exec switch-off install-patchperl lib
 /;
 
 my @perlbrew_options = qw/
  -h --help -f --force -j -n --notest -q --quiet -v --verbose --as -D -U -A
 /;
 
+my @lib_subcommands = qw/
+    create delete
+/;
+
+my @alias_subcommands = qw/
+    create rename delete
+/;
+
 sub should_activate {
     return [ grep { command_in_path($_) } qw/perlbrew/ ];
+}
+
+sub _extract_perl {
+    my ( $perl ) = @_;
+
+    $perl =~ s/\@.*//;
+    return $perl
+}
+
+sub _extract_lib {
+    my ( $perl ) = @_;
+
+    $perl =~ s/.*\@//;
+
+    return $perl;
+}
+
+sub _get_perls {
+    my @perls = split /\n/, qx(perlbrew list);
+    my ( $current_perl ) = grep { /^\*\s*/ } @perls;
+    ( $current_perl )    = $current_perl =~ /^\*\s*(\S+)/;
+
+    $current_perl = _extract_perl($current_perl);
+
+    return ( $current_perl, map { /^\*?\s*(?<name>\S+)/; $+{'name'} } @perls );
 }
 
 sub complete {
@@ -45,15 +78,67 @@ sub complete {
                 $r->candidates(grep { /^\Q$word\E/ }
                     ( @perlbrew_commands, @perlbrew_options ));
             }
-            when(qr/^switch|env|use|uninstall|alias$/) {
-                my @perls = split /\n/, qx(perlbrew list);
-                @perls = map { /^\*?\s*(?<name>\S+)/; $+{'name'} } @perls;
+            when(qr/^(?:switch|env|use)$/) {
+                my ( $current_perl, @perls ) = _get_perls();
+                my @libs = map { '@' . _extract_lib($_) }
+                    grep { /^\Q$current_perl\E\@/ } @perls;
+                $r->candidates(grep { /^\Q$word\E/ } ( @perls, @libs ));
+            }
+            when('uninstall') {
+                my ( undef, @perls ) = _get_perls();
                 $r->candidates(grep { /^\Q$word\E/ } @perls);
             }
             when('install') {
                 my @perls = split /\n/, qx(perlbrew available);
                 @perls = map { /^i?\s*(?<name>.*)/; $+{'name'}  } @perls;
+                push @perls, 'perl-blead';
                 $r->candidates(grep { /^\Q$word\E/ } @perls);
+            }
+            when('lib') {
+                my ( $subcommand ) = grep { $_ !~ /^-/ } @args[ 1 .. $#args ];
+
+                $subcommand //= '';
+
+                if($subcommand eq $word) {
+                    $r->candidates(grep { /^\Q$word\E/ } @lib_subcommands);
+                } else {
+                    if($subcommand eq 'delete') {
+                        my ( $current_perl, @perls ) = _get_perls();
+                        my @full_libs    = grep { /\@/ } @perls;
+                        my @current_libs = map { '@' . _extract_lib($_) }
+                            grep { /^\Q$current_perl\E\@/ } @perls;
+
+                        $r->candidates(grep { /^\Q$word\E/ } ( @full_libs, @current_libs ));
+                    } else {
+                        $r->candidates(); # we can't predict what you name your
+                                          # libs!
+                    }
+                }
+            }
+            when('alias') {
+                my @words = grep { $_ !~ /^-/ } @args[ 1.. $#args ];
+
+                my $subcommand = $words[0] // '';
+
+                if($subcommand eq $word) {
+                    $r->candidates(grep { /^\Q$word\E/ } @alias_subcommands);
+                } else {
+                    if($subcommand eq 'create') {
+                        my $name = $words[1] // '';
+
+                        if($name eq $word) {
+                            my ( undef, @perls ) = _get_perls();
+                            @perls               = grep { $_ !~ /\@/ } @perls;
+
+                            $r->candidates(grep { /^\Q$word\E/ } @perls);
+                        } else {
+                            $r->candidates();
+                        }
+                    } else {
+                        $r->candidates(); # unfortunately, we can't list
+                                          # aliases separately yet =(
+                    }
+                }
             }
             default {
                 # all other commands (including unrecognized ones) get
@@ -76,7 +161,7 @@ Bash::Completion::Plugins::perlbrew - Bash completion for perlbrew
 
 =head1 VERSION
 
-version 0.04
+version 0.05
 
 =head1 DESCRIPTION
 
@@ -105,7 +190,7 @@ Rob Hoelz <rob@hoelz.ro>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Rob Hoelz.
+This software is copyright (c) 2012 by Rob Hoelz.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
@@ -113,7 +198,7 @@ the same terms as the Perl 5 programming language system itself.
 =head1 BUGS
 
 Please report any bugs or feature requests on the bugtracker website
-http://github.com/hoelzro/bash-completion-plugins-perlbrew/issues
+https://github.com/hoelzro/bash-completion-plugins-perlbrew/issues
 
 When submitting a bug or request, please include a test-file or a
 patch to an existing test-file that illustrates the bug or desired
