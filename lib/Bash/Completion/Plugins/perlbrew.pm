f10f3f795724c592cc970ae71e0c44dee6c4ac78
Rob Hoelz <rob@hoelz.ro>
2013-06-02 12:30:36 +0000

    Import of RHOELZ/Bash-Completion-Plugins-perlbrew-0.10 from CPAN.
    gitpan-cpan-distribution: Bash-Completion-Plugins-perlbrew
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         RHOELZ/Bash-Completion-Plugins-perlbrew-0.10.tar.gz
    gitpan-cpan-author:       RHOELZ
    gitpan-cpan-maturity:     released

--- lib/Bash/Completion/Plugins/perlbrew.pm
+++ lib/Bash/Completion/Plugins/perlbrew.pm
@@ -1,13 +1,12 @@
 ## no critic (RequireUseStrict)
 package Bash::Completion::Plugins::perlbrew;
 {
-  $Bash::Completion::Plugins::perlbrew::VERSION = '0.09';
+  $Bash::Completion::Plugins::perlbrew::VERSION = '0.10';
 }
 
 ## use critic (RequireUseStrict)
 use strict;
 use warnings;
-use feature 'switch';
 use parent 'Bash::Completion::Plugin';
 
 use Bash::Completion::Utils qw(command_in_path prefix_match);
@@ -15,13 +14,14 @@ use Bash::Completion::Utils qw(command_in_path prefix_match);
 my @perlbrew_commands = qw/
 init    install list use           switch    mirror    off
 version help    env  install-cpanm available uninstall self-upgrade
-alias exec switch-off install-patchperl lib install-ack
-list-modules info download upgrade-perl
+alias exec switch-off install-patchperl lib
+list-modules info download upgrade-perl install-multiple
 /;
 
 my @perlbrew_options = qw/
  -h --help -f --force -j -n --notest -q --quiet -v --verbose --as -D -U -A
- --with --switch
+ --with --switch --both --common-variations --all-variations
+ --noman --thread --multi --64int --64all --ld --debug --sitecustomize
 /;
 
 my @lib_subcommands = qw/
@@ -75,87 +75,79 @@ sub complete {
 
         my $command = $args[0] // '';
 
-        given($command) {
-            when($command eq $word) {
-                $r->candidates(prefix_match($word, @perlbrew_commands,
-                    @perlbrew_options));
-            }
-            when(qr/^(?:switch|env|use)$/) {
-                my ( $current_perl, @perls ) = _get_perls();
-                my @libs = map { '@' . _extract_lib($_) }
-                    prefix_match($current_perl . '@', @perls);
-                $r->candidates(prefix_match($word, @perls, @libs));
-            }
-            when('uninstall') {
-                my ( undef, @perls ) = _get_perls();
-                @perls = grep { !/\@/ } @perls;
-                $r->candidates(prefix_match($word, @perls));
-            }
-            when(qr/^(?:install|download)$/) {
-                my @perls = split /\n/, qx(perlbrew available);
-                @perls = map { /^i?\s*(?<name>.*)/; $+{'name'}  } @perls;
-                push @perls, 'perl-blead';
-                push @perls, 'perl-stable';
-                foreach my $perl (@perls) {
-                    if($perl =~ /^perl-/) {
-                        my $copy = $perl;
-                        $copy    =~ s/^perl-//;
-                        push @perls, $copy;
-                    }
+        if($command eq $word) {
+            $r->candidates(prefix_match($word, @perlbrew_commands,
+                @perlbrew_options));
+        } elsif($command =~ /^(?:switch|env|use)$/) {
+            my ( $current_perl, @perls ) = _get_perls();
+            my @libs = map { '@' . _extract_lib($_) }
+                prefix_match($current_perl . '@', @perls);
+            $r->candidates(prefix_match($word, @perls, @libs));
+        } elsif($command eq 'uninstall') {
+            my ( undef, @perls ) = _get_perls();
+            @perls = grep { !/\@/ } @perls;
+            $r->candidates(prefix_match($word, @perls));
+        } elsif($command =~ /^(?:install|download|install-multiple)$/) {
+            my @perls = split /\n/, qx(perlbrew available);
+            @perls = map { /^i?\s*(?<name>.*)/; $+{'name'}  } @perls;
+            push @perls, 'perl-blead';
+            push @perls, 'perl-stable';
+            foreach my $perl (@perls) {
+                if($perl =~ /^perl-/) {
+                    my $copy = $perl;
+                    $copy    =~ s/^perl-//;
+                    push @perls, $copy;
                 }
-                $r->candidates(prefix_match($word, @perls));
             }
-            when('lib') {
-                my ( $subcommand ) = grep { $_ !~ /^-/ } @args[ 1 .. $#args ];
+            $r->candidates(prefix_match($word, @perls));
+        } elsif($command eq 'lib') {
+            my ( $subcommand ) = grep { $_ !~ /^-/ } @args[ 1 .. $#args ];
 
-                $subcommand //= '';
+            $subcommand //= '';
 
-                if($subcommand eq $word) {
-                    $r->candidates(prefix_match($word, @lib_subcommands));
+            if($subcommand eq $word) {
+                $r->candidates(prefix_match($word, @lib_subcommands));
+            } else {
+                if($subcommand eq 'delete') {
+                    my ( $current_perl, @perls ) = _get_perls();
+                    my @full_libs    = grep { /\@/ } @perls;
+                    my @current_libs = map { '@' . _extract_lib($_) }
+                        prefix_match($current_perl . '@', @perls);
+
+                    $r->candidates(prefix_match($word, @full_libs, @current_libs));
                 } else {
-                    if($subcommand eq 'delete') {
-                        my ( $current_perl, @perls ) = _get_perls();
-                        my @full_libs    = grep { /\@/ } @perls;
-                        my @current_libs = map { '@' . _extract_lib($_) }
-                            prefix_match($current_perl . '@', @perls);
-
-                        $r->candidates(prefix_match($word, @full_libs, @current_libs));
-                    } else {
-                        $r->candidates(); # we can't predict what you name your
-                                          # libs!
-                    }
+                    $r->candidates(); # we can't predict what you name your
+                                      # libs!
                 }
             }
-            when('alias') {
-                my @words = grep { $_ !~ /^-/ } @args[ 1.. $#args ];
+        } elsif($command eq 'alias') {
+            my @words = grep { $_ !~ /^-/ } @args[ 1.. $#args ];
 
-                my $subcommand = $words[0] // '';
+            my $subcommand = $words[0] // '';
 
-                if($subcommand eq $word) {
-                    $r->candidates(prefix_match($word, @alias_subcommands));
+            if($subcommand eq $word) {
+                $r->candidates(prefix_match($word, @alias_subcommands));
+            } else {
+                if($subcommand eq 'create') {
+                    my $name = $words[1] // '';
+
+                    if($name eq $word) {
+                        my ( undef, @perls ) = _get_perls();
+                        @perls               = grep { $_ !~ /\@/ } @perls;
+
+                        $r->candidates(prefix_match($word, @perls));
+                    } else {
+                        $r->candidates();
+                    }
                 } else {
-                    if($subcommand eq 'create') {
-                        my $name = $words[1] // '';
-
-                        if($name eq $word) {
-                            my ( undef, @perls ) = _get_perls();
-                            @perls               = grep { $_ !~ /\@/ } @perls;
-
-                            $r->candidates(prefix_match($word, @perls));
-                        } else {
-                            $r->candidates();
-                        }
-                    } else {
-                        $r->candidates(); # unfortunately, we can't list
-                                          # aliases separately yet =(
-                    }
+                    $r->candidates(); # unfortunately, we can't list
+                                      # aliases separately yet =(
                 }
             }
-            default {
-                # all other commands (including unrecognized ones) get
-                # no completions
-                $r->candidates();
-            }
+        } else {
+            # all other commands (including unrecognized ones) get
+            # no completions
+            $r->candidates();
         }
     }
 }
@@ -166,8 +158,6 @@ sub generate_bash_setup {
 
 1;
 
-
-
 =pod
 
 =head1 NAME
@@ -176,7 +166,7 @@ Bash::Completion::Plugins::perlbrew - Bash completion for perlbrew
 
 =head1 VERSION
 
-version 0.09
+version 0.10
 
 =head1 DESCRIPTION
 
@@ -220,7 +210,6 @@ patch to an existing test-file that illustrates the bug or desired
 feature.
 
 =cut
-
 
 __END__
 
